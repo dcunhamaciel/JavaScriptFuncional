55836c4157f22a9fdf5178381f0344a0f2c0b9c4
dcunhamaciel <dcunhamaciel@gmail.com>
2024-02-08 21:34:08 -0300

    OO #03

---
+++ 01. Função/oo_3.js
@@ -0,0 +1,43 @@
+function Produto(nome, preco, desconto = 0.15) {
+    this.nome = nome
+    this.preco = preco
+    this._desconto = desconto
+
+    this.precoFinal = function() {
+        return this.preco * (1 - this._desconto)
+    }
+}
+
+Produto.prototype.log = function() {
+    console.log(`Nome: ${this.nome} - Preço: R$ ${this.preco}`)
+}
+
+Object.defineProperty(Produto.prototype, 'desconto', {
+    get: function() {
+        return this._desconto
+    },
+    set: function(novoDesconto) {
+        if (novoDesconto >= 0 && novoDesconto <= 1) {
+            this._desconto = novoDesconto
+        }
+    }
+})
+
+Object.defineProperty(Produto.prototype, 'descontoString', {
+    get: function() {
+        return `${this._desconto * 100}% de desconto`
+    }
+})
+
+const p1 = new Produto('Caneta', 8.59)
+console.log(p1.nome)
+p1.log()
+
+const p2 = new Produto('Geladeira', 3000.0)
+console.log(p2.preco)
+console.log(p2.precoFinal())
+
+p2.desconto = 0.9
+
+console.log(p2.desconto)
+console.log(p2.descontoString)
